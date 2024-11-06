6019a9c1a572ae5f8611408beca59a8ca609dade
martyu1994 <martyu1994@gmail.com>
2024-11-06 18:39:02 +0800

    actS88

--- models/Blog.js
+++ models/Blog.js
@@ -1,25 +1,17 @@
+// models/Blog.js
 const mongoose = require('mongoose');
 
-// Updated comment schema to reference only userId
 const commentSchema = new mongoose.Schema({
-    userId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User' 
-    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     comment: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
-// Blog schema
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    author: {
-        username: { type: String, required: false },
-        email: { type: String, required: false }
-    },
-    comments: [commentSchema],  // An array of comments, each referencing a user by userId
-    creationDate: { type: Date, default: Date.now }  // Explicit creation date
-}, {});
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    comments: [commentSchema],
+}, { timestamps: true });
 
 module.exports = mongoose.model('Blog', blogSchema);
